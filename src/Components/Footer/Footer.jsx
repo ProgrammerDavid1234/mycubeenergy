7a1ae815d96e844932d3ac1036aad5fbd2e1e048
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-09-06 11:40:26 +0100

    first commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,13 +6,20 @@ const Footer = () => {
   return (
     <div className={styles.footerContainer}>
       <div className={styles.footerContent}>
+        {/* Align Logo and Info */}
         <div className={styles.footerSection}>
-          <img src={logo} alt="Logo" className={styles.footerLogo} />
-          <div className={styles.subscribeSection}>
-            <p className={styles.footerText}>Funding freemium long tail hypotheses first mover advantage assets ownership</p>
-          </div>
-          <a href="#" className={styles.contactInfo}>agensi@mail.com</a>
-          <a href="#" className={styles.contactInfo}>+ 12 3456 7890</a>
+          {/* <div className={styles.logoAndInfo}> */}
+            {/* <img src={logo} alt="Logo" className={styles.footerLogo} /> */}
+            <h3 className={styles.footerSectionTitle}><img src={logo} alt="" /></h3>
+
+            <div>
+              <p className={styles.footerText}>
+                Funding freemium long tail hypotheses first mover advantage assets ownership
+              </p>
+              <a href="mailto:agensi@mail.com" className={styles.contactInfo}>agensi@mail.com</a>
+              <a href="tel:+1234567890" className={styles.contactInfo}>+ 12 3456 7890</a>
+            </div>
+          {/* </div> */}
         </div>
 
         <div className={styles.footerSection}>
